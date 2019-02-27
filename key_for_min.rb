ec427d5a33eb9602c0e70437af8cf1da4645ca0b
Natasha Chernis <natashachernis@gmail.com>
2019-02-27 05:08:22 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,6 +7,15 @@ def key_for_min_value(name_hash)
 
   name_hash_array = name_hash.collect {|key, value| value}
 
+  num = name_hash_array[0]
 
+  name_hash_array.each do |value|
+    num = value if value < num 
   end
+
+  name_hash.collect do |key, value|
+    key if value == num
+  end
+
+
 end
