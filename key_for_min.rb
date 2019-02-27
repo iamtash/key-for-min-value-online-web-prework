33de72f5deb17885b1daa8b5befa17f41d8ca4c6
Natasha Chernis <natashachernis@gmail.com>
2019-02-27 05:10:22 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ hash = {:blake => 500, :ashley => 2, :adam => 1}
 def key_for_min_value(name_hash)
 
 
-  binding.pry
+  
   name_hash_array = name_hash.collect {|key, value| value}
 
   num = name_hash_array[0]
